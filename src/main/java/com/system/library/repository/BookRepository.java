4cc686a09d07b08d789c6664d50318237540fef9
MD Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-07-29 21:49:19 +0600

    new file

--- src/main/java/com/system/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.library.repository;
-
-import com.system.library.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
--- src/main/java/com/system/library/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.library.repository;
-
-import com.system.library.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
